b8dbdb578115c31429c8044332e2683740e7cf32
alejorodriguez96 <54213247+alejorodriguez96@users.noreply.github.com>
2019-10-01 18:51:40 -0300

    Corrección errores grosos
    Esta todavia lejos de compilar

--- hash.c
+++ hash.c
@@ -1,8 +1,15 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "hash.h"
+
 
 #define OCUPADO 1
-#define BORRADO -1
+#define BORRADO 2
 #define VACIO 0
 #define TAM_INICIAL 30
+#define CARGA_MAX 70
+#define CARGA_MIN 30
 
 typedef struct campo{
   char* clave;
@@ -22,10 +29,37 @@ struct hash_iter{
   campo_t campo_act;
   size_t posicion;
 };
-typedef void (*hash_destruir_dato_t)(void *);
 
-int hash_obtener_posicion(hash_t* hash, char* clave, int* existencia){
-  int indice = fhash(clave);
+size_t fhash(const char *str, size_t tam){
+    size_t hash = 5381;
+    size_t c;
+
+    while ((c = *str++)){
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return hash%tam;
+}
+
+
+bool hash_redimensionar(hash_t* hash, size_t tam){
+  campo_t* campos = calloc(tam, sizeof(campo_t));
+  if (campos == NULL) return false;
+  for (int i=0; i<hash->capacidad;i++){
+    if (hash->campos[i].estado != OCUPADO) continue;
+    campos[fhash(hash->campos[i].clave, hash->capacidad)] = hash->campos[i];
+  }
+  hash->capacidad = tam;
+  hash->campos = campos;
+  return true;
+}
+
+
+
+
+//Si la clave se encuentra en el hash devuelve su posicion, sino devuelve la
+//posicion del espacio vacio o borrado mas proximo.
+int hash_obtener_posicion(hash_t* hash,const char* clave, int* existencia){
+  int indice = (int)fhash(clave, hash->capacidad);
   int pos_act = 0;
   for (int i=indice; i<hash->capacidad; i++){
     pos_act = (indice+i)%hash->capacidad;
@@ -33,14 +67,14 @@ int hash_obtener_posicion(hash_t* hash, char* clave, int* existencia){
       *existencia = 1;
       return pos_act;
     }
-    if (hash->campos[pos_act].estado == BORRADO) continue;
-    if (hash->campos[pos_act].estado == VACIO){
+  }
+  for (int i=indice; i<hash->capacidad; i++){
+    pos_act = (indice+i)%hash->capacidad;
+    if (hash->campos[pos_act].estado != OCUPADO){
       *existencia = 0;
-      return 0;
+      return pos_act;
     }
   }
-  *existencia = 0;
-  return 0;
 }
 
 hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
@@ -53,6 +87,9 @@ hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
 }
 
 bool hash_guardar(hash_t *hash, const char *clave, void *dato){
+  if ((hash->cantidad/hash->capacidad)*100 >= CARGA_MAX){
+    hash_redimensionar(hash, hash->capacidad*2);
+  }
   int existencia;
   int pos = hash_obtener_posicion(hash, clave, &existencia);
   campo_t campo;
@@ -62,37 +99,24 @@ bool hash_guardar(hash_t *hash, const char *clave, void *dato){
     hash->campos[pos].valor = dato;
     return true;
   }
-  bool pertenencia = hash_pertenece(hash, clave);
-  for (int i = fhash(clave); i<hash->capacidad; i++){
-    if (hash->campos[i].clave == clave){
-      hash->campos[i] = campo;
-      return true;
-    }
-    if (!pertenencia && hash->campos[i].estado != OCUPADO){
-      hash->campos[i] = campo;
-      return true;
-    }
-  }
-  for (int i = 0; i<fhash(clave); i++){
-    if (hash->campos[i].clave == clave){
-      hash->campos[i] = campo;
-      return true;
-    }
-    if (!pertenencia && hash->campos[i].estado != OCUPADO){
-      hash->campos[i] = campo;
-      return true;
-    }
-  }
-  return false;
+  hash->campos[pos] = campo;
+  hash->cantidad++;
+  return true;
 }
 
 void *hash_borrar(hash_t *hash, const char *clave){
   int existencia;
   int pos = hash_obtener_posicion(hash, clave, &existencia);
+  void* valor = NULL;
   if (existencia == 1){
     hash->campos[pos].estado = BORRADO;
-    return hash->campos[pos].valor;
+    valor = hash->campos[pos].valor;
+    if ((hash->cantidad / hash->capacidad)*100 <= CARGA_MIN){
+      hash_redimensionar(hash, hash->capacidad/2);
+    }
+    hash->cantidad--;
   }
+  return valor;
 }
 
 void *hash_obtener(const hash_t *hash, const char *clave){
@@ -103,8 +127,8 @@ void *hash_obtener(const hash_t *hash, const char *clave){
 
 bool hash_pertenece(const hash_t *hash, const char *clave){
   int existencia;
-  int pos = hash_obtener_posicion(hash, clave, &existencia);
-  return *existencia == 1;
+  hash_obtener_posicion(hash, clave, &existencia);
+  return existencia == 1;
 }
 
 size_t hash_cantidad(const hash_t *hash){
